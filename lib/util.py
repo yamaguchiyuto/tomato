068202fab242962cd3e8da374c32d79c6edd21ba
Yuto Yamaguchi <yuto@tuna.kde.cs.tsukuba.ac.jp>
2013-06-14 19:30:11 +0900

    removed unnecessary method: hubeny_distance

--- lib/util.py
+++ lib/util.py
@@ -31,18 +31,3 @@ def calc_variance(center, points):
         err_sum += (p[0]-center[0])**2
         err_sum += (p[1]-center[1])**2
     return err_sum / (2 * (n-1))
-
-def hubeny_distance(p, q):
-    latd = rad(p[0] - q[0])
-    longd = rad(p[1] - q[1])
-    latm = rad(p[0] + q[0]) / 2
-    a = 6377397.155
-    b = 6356079.000
-    e2 = 0.00667436061028297
-    W = math.sqrt(1 - e2 * math.sin(latm)**2)
-    M = 6334832.10663254 / W**3
-    N = a / W
-    d = math.sqrt((latd*M)**2 + (longd*N*math.cos(latm))**2)
-    return d
-
-
